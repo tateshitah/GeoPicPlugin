c5bc81055d1049f8e93abe17185d80e555555487
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2017-11-10 22:12:05 +0900

    implementation would be almost completed.

--- src/main/java/org/braincopy/jspwiki/plugin/GeoPicPlugin.java
+++ src/main/java/org/braincopy/jspwiki/plugin/GeoPicPlugin.java
@@ -50,7 +50,7 @@ public class GeoPicPlugin implements WikiPlugin {
 			parentPages = new String[1];
 			parentPages[0] = context.getName();
 		}
-		result +="<form action=\"/gpupload\" enctype=\"multipart/form-data\" method=\"post\">\n";
+		result += "<form action=\"gpupload\" enctype=\"multipart/form-data\" method=\"post\">\n";
 
 		result += "\t<p><label for=\"parent_page\">Select parent page</label>\n";
 		result += "\t<select id=\"parent_page\" name=\"parent_page\">\n";
@@ -60,12 +60,17 @@ public class GeoPicPlugin implements WikiPlugin {
 		}
 		result += "\t</select>\n\t</p>\n";
 		result += "\t<p>\n\t<label for=\"name\">Name:</label>\n";
-		result += "\t<input type=\"text\" name=\"name\" id=\"name\" size=\"40\" /></p>\n";
+		result += "\t<input type=\"text\" name=\"name\" id=\"name\" size=\"40\" required /></p>\n";
 		result += "\t<p>\n\t<label for=\"attachfilename\">Select file:</label>\n";
-		result += "\t<input type=\"file\" name=\"content\" id=\"attachfilename\" size=\"60\" />\n";
+		result += "\t<input type=\"file\" name=\"content\" id=\"attachfilename\" size=\"60\" required />\n";
 		result += "\t</p>\n\t<p>";
-		result += "\t<label for=\"location\">Location:</label>\n";
-		result += "\t<input type=\"text\" name=\"location\" id=\"location\" size=\"40\" />\n";
+		// result += "\t<label for=\"location\">Location:</label>\n";
+		// result += "\t<input type=\"text\" name=\"location\" id=\"location\"
+		// size=\"40\" /><br/>\n";
+		result += "\t<label for=\"lat\">Latitude:</label>\n";
+		result += "\t<input type=\"number\" name=\"lat\" id=\"lat\" size=\"15\" min=\"-90.0\" max=\"90.0\" step=\"0.0000000001\" required />\n";
+		result += "\t<label for=\"lon\">Longitude:</label>\n";
+		result += "\t<input type=\"number\" name=\"lon\" id=\"lon\" size=\"15\" min=\"-180.0\" max=\"180.0\" step=\"0.0000000001\" required />\n";
 		result += "\t<button type=\"button\" onclick=\"getLocation()\">Get Location</button>\n";
 		result += "\t</p>\n\t<script>\n";
 		result += "\tfunction getLocation() {\n";
@@ -75,12 +80,21 @@ public class GeoPicPlugin implements WikiPlugin {
 		result += "\tfunction update(position) {\n";
 		result += "\t\tvar lat = position.coords.latitude;\n";
 		result += "\t\tvar lng = position.coords.longitude;\n";
-		result += "\t\tdocument.getElementById('location').value = lat + ', ' + lng;}\n";
+		// result += "\t\tdocument.getElementById('location').value = lat + ', ' +
+		// lng;\n";
+		result += "\t\tdocument.getElementById('lat').value = lat;\n";
+		result += "\t\tdocument.getElementById('lon').value = lng;}\n";
 		result += "\t</script>\n";
+		result += "\t<p><label for=\"description\">Description:</label><br/>\n";
+		result += "\t<textarea name=\"description\" id=\"description\" rows=\"3\" cols=\"60\" ></textarea></p>\n";
 		result += "\t<p>\n\t<input type=\"submit\" value=\"Upload\">\n";
 		result += "\t<input type=\"reset\" value=\"Reset\">\n\t</p>\n";
-		
-		result +="</form>\n";
+
+		result += "</form>\n";
+
+		// for Debug
+		// WikiEngine engine = context.getEngine();
+		// result += engine.getWorkDir() + ": " + engine.toString();
 
 		return result;
 	}
--- src/main/java/org/braincopy/jspwiki/plugin/GeoPicServlet.java
+++ src/main/java/org/braincopy/jspwiki/plugin/GeoPicServlet.java
@@ -1,134 +1,364 @@
 package org.braincopy.jspwiki.plugin;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.ProgressListener;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.apache.wiki.PageManager;
+import org.apache.wiki.WikiContext;
 import org.apache.wiki.WikiEngine;
+import org.apache.wiki.WikiPage;
+import org.apache.wiki.api.exceptions.ProviderException;
+import org.apache.wiki.api.exceptions.RedirectException;
+import org.apache.wiki.attachment.AttachmentServlet;
+import org.apache.wiki.ui.progress.ProgressItem;
+import org.apache.wiki.util.TextUtil;
 
 @WebServlet("/gpupload")
-public class GeoPicServlet extends HttpServlet {
+public class GeoPicServlet extends AttachmentServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 	private WikiEngine m_engine;
+
+	/**
+	 * The maximum size that an attachment can be.
+	 */
+	private int m_maxSize = Integer.MAX_VALUE;
+
+	private static final Logger log = Logger.getLogger(AttachmentServlet.class);
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
+		m_engine = WikiEngine.getInstance(config);
 		super.init(config);
+	}
+
+	/**
+	 * Uploads a specific mime multipart input set, intercepts exceptions.
+	 *
+	 * @param req
+	 *            The servlet request
+	 * @return The page to which we should go next.
+	 * @throws RedirectException
+	 *             If there's an error and a redirection is needed
+	 * @throws IOException
+	 *             If upload fails
+	 * @throws FileUploadException
+	 */
+	@Override
+	protected String upload(HttpServletRequest req) throws RedirectException, IOException {
+		String msg = "";
+		String attName = "(unknown)";
+		String errorPage = m_engine.getURL(WikiContext.ERROR, "", null, false); // If something bad happened, Upload
+																				// should be able to take care of
+																				// most stuff
+
+		String nextPage = errorPage;
+
+		String progressId = req.getParameter("progressid");
+
+		// Check that we have a file upload request
+		if (!ServletFileUpload.isMultipartContent(req)) {
+			throw new RedirectException("Not a file upload", errorPage);
+		}
+
 		try {
-			m_engine = WikiEngine.getInstance(config);
-		} catch (org.apache.wiki.InternalWikiException e) {
-			// once failed, try again just once. it's because of Servlet 3.0. JSPWiki's
-			// engine seems to try to check web.xml but no need for servlet 3.0.
-			m_engine = WikiEngine.getInstance(config);
+			FileItemFactory factory = new DiskFileItemFactory();
+
+			// Create the context _before_ Multipart operations, otherwise
+			// strict servlet containers may fail when setting encoding.
+			WikiContext context = m_engine.createContext(req, WikiContext.ATTACH);
+
+			UploadListener pl = new UploadListener();
+
+			m_engine.getProgressManager().startProgress(pl, progressId);
+
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			upload.setHeaderEncoding("UTF-8");
+			if (!context.hasAdminPermissions()) {
+				upload.setFileSizeMax(m_maxSize);
+			}
+			upload.setProgressListener(pl);
+			List<FileItem> items = upload.parseRequest(req);
+
+			String wikipage = null;
+			String changeNote = null;
+			FileItem actualFile = null;
+
+			///// added by Hiroaki Tateshita
+			String name = null;
+			String parent = null;
+			// String locationStr = null;
+			float lat = Float.MAX_VALUE;
+			float lon = Float.MAX_VALUE;
+			String description = null;
+			/////
+
+			for (FileItem item : items) {
+				if (item.isFormField()) {
+					if (item.getFieldName().equals("page")) {
+						//
+						// FIXME: Kludge alert. We must end up with the parent page name,
+						// if this is an upload of a new revision
+						//
+
+						wikipage = item.getString("UTF-8");
+						int x = wikipage.indexOf("/");
+
+						if (x != -1)
+							wikipage = wikipage.substring(0, x);
+					} else if (item.getFieldName().equals("changenote")) {
+						changeNote = item.getString("UTF-8");
+						if (changeNote != null) {
+							changeNote = TextUtil.replaceEntities(changeNote);
+						}
+					} else if (item.getFieldName().equals("nextpage")) {
+						nextPage = validateNextPage(item.getString("UTF-8"), errorPage);
+					}
+
+					///// added by Hiroaki Tateshita
+					else if (item.getFieldName().equals("name")) {
+						name = item.getString("UTF-8");
+						if (name == "") {
+							throw new RedirectException("Wrong page name", errorPage);
+						}
+					} else if (item.getFieldName().equals("parent_page")) {
+						parent = item.getString("UTF-8");
+						if (!m_engine.pageExists(parent)) {
+							throw new RedirectException("Wrong parent page name", errorPage);
+						}
+
+						/*
+						 * else if (item.getFieldName().equals("location")) { locationStr =
+						 * item.getString("UTF-8"); if (locationStr.contains(",")) {
+						 * 
+						 * } else { throw new RedirectException("Wrong location", errorPage); } }
+						 */
+					} else if (item.getFieldName().equals("lat")) {
+						lat = Float.parseFloat(item.getString("UTF-8"));
+					} else if (item.getFieldName().equals("lon")) {
+						lon = Float.parseFloat(item.getString("UTF-8"));
+					} else if (item.getFieldName().equals("description")) {
+						description = item.getString("UTF-8");
+					} /////
+
+				} else {
+					actualFile = item;
+				}
+			}
+
+			if (actualFile == null)
+				throw new RedirectException("Broken file upload", errorPage);
+
+			//
+			// FIXME: Unfortunately, with Apache fileupload we will get the form fields in
+			// order. This means that we have to gather all the metadata from the
+			// request prior to actually touching the uploaded file itself. This
+			// is because the changenote appears after the file upload box, and we
+			// would not have this information when uploading. This also means
+			// that with current structure we can only support a single file upload
+			// at a time.
+			//
+			String filename = actualFile.getName();
+
+			///// added by Hiroaki Tateshita
+			if (m_engine.pageExists(name)) {
+				wikipage = addInfo(name, description, lat, lon, filename);
+			} else {
+				wikipage = createNewPage(name, parent, description, lat, lon, filename);
+			}
+			/////
+
+			long fileSize = actualFile.getSize();
+			InputStream in = actualFile.getInputStream();
+
+			try {
+				executeUpload(context, in, filename, nextPage, wikipage, changeNote, fileSize);
+				///// added by Hiroaki Tateshita
+				nextPage = "Wiki.jsp?page=" + URLEncoder.encode(wikipage, "UTF-8");
+				/////
+			} finally {
+				IOUtils.closeQuietly(in);
+			}
+
+		} catch (
+
+		ProviderException e) {
+			msg = "Upload failed because the provider failed: " + e.getMessage();
+			log.warn(msg + " (attachment: " + attName + ")", e);
+
+			throw new IOException(msg);
+		} catch (IOException e) {
+			// Show the submit page again, but with a bit more
+			// intimidating output.
+			msg = "Upload failure: " + e.getMessage();
+			log.warn(msg + " (attachment: " + attName + ")", e);
+
+			throw e;
+		} catch (FileUploadException e) {
+			// Show the submit page again, but with a bit more
+			// intimidating output.
+			msg = "Upload failure: " + e.getMessage();
+			log.warn(msg + " (attachment: " + attName + ")", e);
+
+			throw new IOException(msg, e);
+		} finally {
+			m_engine.getProgressManager().stopProgress(progressId);
+			// FIXME: In case of exceptions should absolutely
+			// remove the uploaded file.
 		}
+
+		return nextPage;
 	}
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	private String addInfo(String name, String description, float lat, float lon, String filename)
+			throws ProviderException {
+		WikiPage page = m_engine.getPage(name);
+		String content = m_engine.getPureText(page);
+		PageManager manager = m_engine.getPageManager();
+		content += "\n*Place\n";
+		content += "[{OSM lat='" + lat + "' lon='" + lon + "'}]\n";
+		content += "*Pic\n[{Image src='" + filename + "' width='300' }]\n";
 
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-
-		out.println(createHTML("GET"));
-
-		out.close();
+		manager.putPageText(page, content);
+		return name;
 	}
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	/**
+	 * This method was added by Hiroaki Tateshita
+	 * 
+	 * @param name
+	 * @param parent
+	 * @param description
+	 * @param lat
+	 * @param lon
+	 * @param filename
+	 * @return the name of wikipage if no exception happens
+	 * @throws ProviderException
+	 */
+	private String createNewPage(String name, String parent, String description, float lat, float lon, String filename)
+			throws ProviderException {
 
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
+		String content = createContent(name, parent, description, lat, lon, filename);
 
-		String[] tempStrs;
-		tempStrs = request.getParameterValues("name");
-		if(tempStrs!=null) {
-			out.println(tempStrs[0]+"\n");
-		}else {
-			out.println("name is null\n");
-		}
-		tempStrs = request.getParameterValues("parent_page");
-		if(tempStrs!=null) {
-			out.println(tempStrs[0]+"\n");
-		}else {
-			out.println("parent_page is null\n");
-		}
-		tempStrs = request.getParameterValues("location");
-		if(tempStrs!=null) {
-			out.println(tempStrs[0]+"\n");
-		}else {
-			out.println("location is null\n");
-		}
-		
-		if(ServletFileUpload.isMultipartContent(request)) {
-			out.println("it should be file upload\n");
-		}else {
-			out.println("it might be not file upload\n");
-		}
-		
-		out.println(createHTML("POST"));
+		WikiPage page = new WikiPage(m_engine, name);
+		PageManager manager = m_engine.getPageManager();
+		manager.putPageText(page, content);
 
-		out.close();
+		return name;
+
 	}
 
-	protected String createHTML(String methodType) {
-		StringBuffer sb = new StringBuffer();
-
-		sb.append("<html>");
-		sb.append("<head>");
-		sb.append("<title>サンプル</title>");
-		sb.append("</head>");
-		sb.append("<body>");
-
-		sb.append("<p>");
-		sb.append(methodType);
-		sb.append("メソッドで呼び出されました</p>");
-
-		sb.append("<p><a href=\"http://localhost:9627/personal/helloworld\">リンク</a></p>");
-
-		sb.append("<form action=\"http://localhost:9627/personal/helloworld\" method=\"get\">");
-		sb.append("<input type=\"submit\" value=\"GETで送信\">");
-		sb.append("</form>");
-
-		sb.append("<form action=\"http://localhost:9627/personal/helloworld\" method=\"post\">");
-		sb.append("<input type=\"submit\" value=\"POSTで送信\">");
-		sb.append("</form>");
-		sb.append(m_engine.getApplicationName());
-
-		sb.append("</body>");
-		sb.append("</html>");
-
-		return (new String(sb));
+	private String createContent(String name, String parent, String description, float lat, float lon,
+			String filename) {
+		String result = "[" + parent + "]" + System.lineSeparator() + "!!!Abstract" + System.lineSeparator();
+		result += description + System.lineSeparator();
+		result += "!!!Topics\n*Place" + System.lineSeparator();
+		result += lat + ", " + lon + System.lineSeparator();
+		result += "[{OSM lat='" + lat + "' lon='" + lon + "'}]" + System.lineSeparator();
+		result += "*Pic\n[{Image src='" + filename + "' width='300' }]" + System.lineSeparator();
+		result += "!!!Reference";
+		return result;
 	}
 
 	/*
-	 * @Override(non-Javadoc)
 	 * 
-	 * @see
-	 * javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest,
-	 * javax.servlet.http.HttpServletResponse)
+	 * public void doPost(HttpServletRequest request, HttpServletResponse response)
+	 * throws IOException, ServletException {
+	 * 
+	 * response.setContentType("text/html; charset=UTF-8"); // PrintWriter out =
+	 * response.getWriter();
+	 * 
+	 * FileItemFactory factory = new DiskFileItemFactory();
+	 * 
+	 * ServletFileUpload upload = new ServletFileUpload(factory);
+	 * upload.setHeaderEncoding("UTF-8"); String name = null; String parent = null;
+	 * String locationStr = null; String description = null;
+	 * 
+	 * try { List<FileItem> items = upload.parseRequest(request); for (FileItem item
+	 * : items) { if (item.isFormField()) { if (item.getFieldName().equals("name"))
+	 * { name = item.getString("UTF-8"); } else if
+	 * (item.getFieldName().equals("parent_page")) { parent =
+	 * item.getString("UTF-8"); } else if (item.getFieldName().equals("location")) {
+	 * locationStr = item.getString("UTF-8"); } else if
+	 * (item.getFieldName().equals("description")) { description =
+	 * item.getString("UTF-8"); } } } } catch (FileUploadException e1) { //
+	 * out.println(e1.getMessage()); e1.printStackTrace(); } if (name != null &&
+	 * parent != null && locationStr != null) { if
+	 * (ServletFileUpload.isMultipartContent(request)) { // out.println(name +
+	 * "<br>" + parent + "<br>" + locationStr + "<br>" + "it // should be file
+	 * upload<br>"); WikiPage page = new WikiPage(m_engine, name); PageManager
+	 * manager = m_engine.getPageManager();
+	 * 
+	 * String content = "[" + parent + "]\n!!!Abstract\n"; content += description +
+	 * "\n"; content += "!!!Topics\n*Place\n"; content += locationStr.trim() + "\n";
+	 * content += "[{OSM lat='" + locationStr.split(",")[0] + "' lon='" +
+	 * locationStr.split(",")[1].trim() + "'}]\n"; content +=
+	 * "*Pic\n[{Image src='***.png' width='300' }]\n"; content += "!!!Reference";
+	 * try { manager.putPageText(page, content); } catch (ProviderException e) { //
+	 * out.append(e.getLocalizedMessage()); e.printStackTrace(); } } else { //
+	 * out.println("it might be not file upload<br>"); } } else { //
+	 * out.println("name or parent or location should be set.<br>"); }
+	 * 
+	 * // out.close();
+	 * 
+	 * // String attach_url = "attach";
+	 * 
+	 * // RequestDispatcher dispatcher = request.getRequestDispatcher(attach_url);
+	 * 
+	 * // dispatcher.forward(request, response); }
 	 */
-	/*
-	 * protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-	 * throws ServletException, IOException { super.doPost(req, resp);
-	 * resp.setContentType("text/html"); PrintWriter out = resp.getWriter();
-	 * out.println("hello " + m_engine.getApplicationName()); }
-	 * 
-	 * protected void doGet(HttpServletRequest request, HttpServletResponse
-	 * response) throws ServletException, IOException {
-	 * 
-	 * response.setContentType("text/html"); PrintWriter out = response.getWriter();
-	 * out.println(""); out.println(""); out.println(new java.util.Date());
-	 * out.println(""); out.println(""); }
+
+	/**
+	 * Validates the next page to be on the same server as this webapp. Fixes
+	 * [JSPWIKI-46].
 	 */
+	private String validateNextPage(String nextPage, String errorPage) {
+		if (nextPage.indexOf("://") != -1) {
+			// It's an absolute link, so unless it starts with our address, we'll
+			// log an error.
+
+			if (!nextPage.startsWith(m_engine.getBaseURL())) {
+				log.warn("Detected phishing attempt by redirecting to an unsecure location: " + nextPage);
+				nextPage = errorPage;
+			}
+		}
+
+		return nextPage;
+	}
+
+	/**
+	 * Provides tracking for upload progress.
+	 * 
+	 */
+	private static class UploadListener extends ProgressItem implements ProgressListener {
+		public long m_currentBytes;
+		public long m_totalBytes;
+
+		public void update(long recvdBytes, long totalBytes, int item) {
+			m_currentBytes = recvdBytes;
+			m_totalBytes = totalBytes;
+		}
+
+		public int getProgress() {
+			return (int) (((float) m_currentBytes / m_totalBytes) * 100 + 0.5);
+		}
+	}
 }
